c9583f62a8bdec906296b1f695cc23fb182f2ed9
tomsknipi <59004739+tomsknipi@users.noreply.github.com>
2020-03-05 15:37:53 +0700

    Add files via upload

---
+++ ver5/MobPresentation.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TomskNipi.Event.MobileClasses.Annotations;
+using TomskNipi.Event.MobileClasses.Enum;
+
+namespace TomskNipi.Event.MobileClasses
+{
+    public class MobPresentation : MobAbstractEntity, INotifyPropertyChanged
+    {
+        public MobPresentation(Guid id, string name, string description, PresentationType valueType, string valueText, Guid eventId)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            ValueType = valueType;
+            ValueText = valueText;
+            EventId = eventId;
+        }
+
+        #region Поля
+
+        private double _viewProgress;
+
+        #endregion
+
+        #region Свойства
+
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Тип презентационного материала
+        /// </summary>
+        public PresentationType ValueType { get; set; }
+
+        /// <summary>
+        /// Link: содержание ссылки
+        /// Document: Имя файла с расширением
+        /// </summary>
+        public string ValueText { get; set; }
+
+        /// <summary>
+        /// Идентификатор мероприятия
+        /// </summary>
+        public Guid EventId { get; set; }
+
+        public string ViewName => string.IsNullOrEmpty(Name) ? string.Empty : Name.Trim();
+
+        public string ViewDescription => string.IsNullOrEmpty(Description) ? string.Empty : Description.Trim();
+
+        public bool HasViewDescription => !string.IsNullOrEmpty(ViewDescription);
+
+        /// <summary>
+        /// Для отображения прогресса загрузки
+        /// </summary>
+        public double ViewProgress
+        {
+            get => _viewProgress;
+            set
+            {
+                if (!_viewProgress.Equals(value))
+                {
+                    _viewProgress = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Для отображения картинки
+        /// </summary>
+        public string ViewImage => ValueType == PresentationType.Link ? "open_in_browser.png" : "cloud_download.png";
+
+        #endregion
+
+        #region Методы
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Реализаторы интерфейса
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
---
+++ ver5/PageEventPresentation.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Event.Classes;
+using Event.Extension;
+using MobileLibrary.Extension;
+using TomskNipi.Event.MobileClasses;
+using TomskNipi.Event.MobileClasses.Enum;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Event
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageEventPresentation
+    {
+        public PageEventPresentation()
+        {
+            InitializeComponent();
+        }
+
+        #region Поля
+
+        private bool _isInitialized;
+
+        #endregion
+
+        #region Свойства
+
+        public ObservableCollection<MobPresentation> PresentationList { get; } = new ObservableCollection<MobPresentation>();
+
+        #endregion
+
+        #region Методы
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            if (_isInitialized)
+                return;
+            if (Config.User != null && Config.MobEvent != null)
+            {
+                IsBusy = true;
+                try
+                {
+                    await Task.Delay(100);
+                    _isInitialized = await RefreshData();
+                }
+                finally
+                {
+                    IsBusy = false;
+                }
+            }
+        }
+
+        public async Task OpenDocument(MobPresentation item)
+        {
+            try
+            {
+                var url = Service.GetPresentationFile(item.Id, item.ValueText);
+                using var webClient = new WebClient();
+                webClient.DownloadProgressChanged += async (sender, args) =>
+                {
+                    item.ViewProgress = args.ProgressPercentage / 100.0f;
+                    await Task.Delay(100);
+                };
+                var file = Path.Combine(CacheDirectory.Get(), item.ValueText);
+                await webClient.DownloadFileTaskAsync(url, file);
+
+                await Launcher.OpenAsync(new OpenFileRequest
+                {
+                    File = new ReadOnlyFile(file)
+                });
+            }
+            finally
+            {
+                item.ViewProgress = 0;
+            }
+        }
+
+        private async Task<bool> RefreshData()
+        {
+            var valid = false;
+            if (Config.User != null && Config.MobEvent != null)
+            {
+                var result = await Service.GetPresentationList(Config.MobEvent.Id);
+                valid = result.Validate(this);
+                if (valid)
+                {
+                    PresentationList.AddRange(result.Data);
+                }
+            }
+            else
+                PresentationList.Clear();
+
+            return valid;
+        }
+
+        #endregion
+
+        #region События
+
+        private async void PresentationListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+            try
+            {
+                await Task.Delay(100);
+                if (e.Item is MobPresentation item)
+                {
+                    switch (item.ValueType)
+                    {
+                        case PresentationType.Link:
+                            await Launcher.TryOpenAsync(item.ValueText);
+                            break;
+                        case PresentationType.Document:
+                            await OpenDocument(item);
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ошибка", ex.Message, "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        #endregion
+    }
+}
